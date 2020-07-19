c12484696c2a16e7df3f9ba21287485e65419c67
Stefan Fiedler <fiedlerstefan93@gmail.com>
2020-07-19 17:19:00 +0200

    added basic login and user fetch logic

--- resources/frontend/app/src/modules/auth.js
+++ resources/frontend/app/src/modules/auth.js
@@ -1,15 +1,49 @@
+import axios from "axios"
+
 export const auth = {
     state: {
-        user: {},
-        authenticated: false
+        token: null,
+        user: null
     },
     mutations: {
-
+        SET_AUTHENTICATION(state, loginResponse) {
+            state.token = loginResponse
+        },
+        SET_USER(state, profileResponse) {
+            state.user = profileResponse
+        }
     },
     actions: {
+        login({commit}, credentials) {
+            return axios.post('/api/auth/login', credentials)
+                .then((response) => {
+                    console.log('response', response)
+                    commit('SET_AUTHENTICATION', response.data)
+                })
+                .catch((response) => {
+                    console.error('response', response)
+                    commit('SET_AUTHENTICATION', null)
+                })
+        },
+        fetchUser({commit, getters}) {
+            let headers = getters.accessToken ? {
 
+                'Authorization': 'bearer ' + getters.accessToken
+            } : {}
+
+            return axios.get('/api/auth/profile', {headers: headers})
+                .then((response) => {
+                    console.log('response', response)
+                    commit('SET_USER', response.data)
+                })
+                .catch((response) => {
+                    console.error('response', response)
+                    commit('SET_USER', null)
+                })
+        }
     },
     getters: {
-        isAuthenticated: state => state.authenticated
+        isAuthenticated: state => state.authenticated,
+        accessToken: state => state.token ? state.token.access_token : null
     }
 }
