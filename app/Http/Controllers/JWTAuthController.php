b89db3208757de08616844b9337af5f055b8e9bf
Stefan Fiedler <fiedlerstefan93@gmail.com>
2020-07-20 15:01:47 +0200

    updated error message format on login

--- app/Http/Controllers/JWTAuthController.php
+++ app/Http/Controllers/JWTAuthController.php
@@ -60,7 +60,7 @@ class JWTAuthController extends Controller
         }
 
         if (!$token = auth()->attempt($validator->validated())) {
-            return response()->json(['error' => 'Unauthorized'], 401);
+            return response()->json(['credentials' => ['Invalid email or password']], 401);
         }
 
         return $this->createNewToken($token);
