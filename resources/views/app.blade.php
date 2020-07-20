0349497efab3a2e419433ff8d35361d965e6a4d4
Stefan Fiedler <fiedlerstefan93@gmail.com>
2020-07-20 16:29:42 +0200

    added csrf-field to header

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,6 +4,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width,initial-scale=1.0">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <link rel="icon" href="/assets/app/favicon.ico">
     <title>app</title>
   <link href="/assets/app/app.js" rel="preload" as="script"></head>
