302d095bff8dac160c98086735df78432f0a06ec
Stefan Fiedler <fiedlerstefan93@gmail.com>
2020-07-20 14:59:00 +0200

    integrated plugins file and auto-registration for Vue components

--- resources/frontend/app/src/main.js
+++ resources/frontend/app/src/main.js
@@ -2,8 +2,12 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+require('./plugins')
 
 Vue.config.productionTip = false;
+
+const files = require.context('./', true, /\.vue$/i)
+files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
 
 new Vue({
     router,
