5da75b2aa1572845d83a41315b70885e209a571c
Stefan Fiedler <fiedlerstefan93@gmail.com>
2020-07-24 20:06:08 +0200

    fixed inconsistent naming

--- resources/frontend/app/src/views/Home.vue
+++ resources/frontend/app/src/views/Home.vue
@@ -30,7 +30,7 @@
                                 <p>Laravel is a free, open-source PHP web framework, created by Taylor Otwell and
                                     intended for the development of web applications following the model–view–controller
                                     architectural pattern and based on Symfony.</p>
-                                <p><a href="https://laravel.com/">Read more</a></p>
+                                <p><a href="https://laravel.com/">Learn more</a></p>
                             </div>
                         </div>
                     </div>
