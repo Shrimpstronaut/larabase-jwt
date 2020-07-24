e11da3c6ab9c4c0af47e5278f81e03a3a5878385
Stefan Fiedler <fiedlerstefan93@gmail.com>
2020-07-24 20:13:42 +0200

    updated title

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width,initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <link rel="icon" href="/assets/app/favicon.ico">
-    <title>app</title>
+    <title>Larabase</title>
   <link href="/assets/app/app.js" rel="preload" as="script"></head>
   <body>
     <noscript>
